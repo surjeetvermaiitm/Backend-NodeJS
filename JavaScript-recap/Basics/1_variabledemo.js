14a3d1db2555b7ace79e212bf170424a240d5e11
surjeetvermaiitm <surjeetverma2017@gmail.com>
2022-12-30 21:47:14 +0530

    javascript

--- JavaScript-recap/Basics/1_variabledemo.js
+++ JavaScript-recap/Basics/1_variabledemo.js
@@ -1,9 +1,23 @@
 var marks = 90;
 var score = 30;
 
-let age = 23;
+// var age = 23;
 let _flag = 0;
 
 const $x$ = 5;
 console.log(marks, score, age, _flag, $x$);
 console.log("surjeet, how are you doing?");
+console.log(`surjeet! Keep going...`);
+
+var marks = 100;
+var name = "surjeet";
+var age;
+
+var salary = undefined;
+marks = null;
+console.log("marks=", marks);
+console.log("name=", name);
+console.log("age=", age);
+console.log("salary=", salary);
+console.log(10 / null);
+console.log(10 < Infinity);
---
+++ Live-session/Javascript/session_1.js
@@ -0,0 +1,6 @@
+console.log("1" + 1);
+console.log("1" + 1 - 1);
+console.log("11" - 1);
+console.log("20" / "2");
+console.log("20" / 2);
+//L to R
