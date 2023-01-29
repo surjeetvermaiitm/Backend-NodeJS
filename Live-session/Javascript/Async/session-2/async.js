f4678340c4ee50e43b497106273278f5fef83c29
surjeetvermaiitm <surjeetverma2017@gmail.com>
2023-01-29 19:24:26 +0530

    js

---
+++ Live-session/Javascript/Async/session-2/async.js
@@ -0,0 +1,92 @@
+// console.log("hi");
+// setTimeout(function exec() {
+//   console.log("hello after 4sec");
+// }, 0000);
+// console.log("end");
+
+// function timeConsumingByLoop() {
+//   console.log("loops sttart");
+//   for (let i = 0; i < 10000000000; i++) {
+//     //
+//   }
+//   console.log("loop ends");
+// }
+
+// function timeConsumingByRunctime() {
+//   console.log("starting time");
+//   setTimeout(function exec() {
+//     //runtime
+//     console.log("completed the timer");
+//   }, 0);
+// }
+// console.log("hi");
+
+// timeConsumingByLoop();
+// timeConsumingByRunctime();
+// timeConsumingByLoop();
+
+// console.log("By");
+//output
+// hi
+// loops sttart
+// loop ends
+// starting time
+// loops sttart
+// loop ends
+// By
+// completed the timer
+
+//
+// function timeConsumingByLoop() {
+//   console.log("loops sttart");
+//   for (let i = 0; i < 1000000000; i++) {
+//     //
+//   }
+//   console.log("loop ends");
+// }
+
+// function timeConsumingByRunctime0() {
+//   console.log("starting time0");
+//   setTimeout(function exec() {
+//     //runtime
+//     console.log("completed the timer0");
+//   }, 5000);
+// }
+// function timeConsumingByRunctime1() {
+//   console.log("starting time1");
+//   setTimeout(function exec() {
+//     //runtime
+//     console.log("completed the timer1");
+//   }, 0);
+// }
+// function timeConsumingByRunctime2() {
+//   console.log("starting time2");
+//   setTimeout(function exec() {
+//     //runtime
+//     console.log("completed the timer2");
+//   }, 200);
+// }
+// console.log("hi");
+
+// timeConsumingByLoop();
+
+// timeConsumingByRunctime0();
+// timeConsumingByRunctime1();
+// timeConsumingByRunctime2();
+// timeConsumingByLoop();
+// console.log("By");
+// output
+// hi
+// loops sttart
+// loop ends
+// starting time0
+// starting time1
+// starting time2
+// loops sttart
+// loop ends
+// By
+// completed the timer1
+// completed the timer2
+// completed the timer0
+
+//explore console.log
