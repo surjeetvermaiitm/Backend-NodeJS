6f8cc4ab16a8213f769dd9385bdc0d4d96459d22
surjeetvermaiitm <surjeetverma2017@gmail.com>
2023-03-02 11:17:43 +0530

    2-march

---
+++ Live-session/Javascript/Async/session-8-iterator-road-to-async-await/closures_demo.js
@@ -0,0 +1,20 @@
+// function DO(task) {
+//   // local to DO is task
+//   setTimeout(function exec() {
+//     console.log(task);
+//   }, 2000);
+// }
+
+// DO("dkfbjh");
+// console.log("end");
+
+// for (let i = 0; i < 4; i++) {
+//   setTimeout(() => {
+//     console.log(i);
+//   });
+// }
+for (var i = 0; i < 4; i++) {
+  setTimeout(() => {
+    console.log(i);
+  }, 2000);
+}
